b8e136a2312495ee5977bae7f324d79bfb43aabb
Estelle <esmonier01@gmail.com>
2021-01-15 08:47:57 +0100

    threadpool instancié (avec erreurs) + facteurs internes sur l'offre et la demade rajoutés

--- market.py
+++ market.py
@@ -4,6 +4,7 @@ import random
 import time
 import signal
 import sysv_ipc
+import concurrent.futures
 
 from politics import *
 from economics import *
@@ -44,13 +45,19 @@ class Market(Process):
         self.coefDevise = 0.5
         self.coefPenurie = 0.5
 
-        #coef et présence des facteurs internes (météo)
+        #coef et présence des facteurs internes (météo et offre/demande)
         self.coefMeteo = 0.5
         self.sign_meteo = 0
+        self.sign_baisse = 0
+        self.coefBaisse = 0.1
+        self.sign_stonks = 0
+        self.coefStronks = 0.1
 
         #affichage du prix du jour
         self.sign_affPrix = 0
-        
+
+        #lancement du thread pool
+        self.sign_transac = 0        
 
         #Gestion du jour :
         signal.signal(signal.SIGUSR1, self.handler)
@@ -78,8 +85,9 @@ class Market(Process):
         #Calcul du nouveau prix de l'energie
             self.sign_affPrix = 1
 
-        #Gestion des transactions
-            self.transactions()
+        #Gestion des transactions entre home et market
+            self.sign_transac = 1
+
         if sig == signal.SIGTERM:
             #Il y'a une tension diplomatique
             self.sign_tension = 1 
@@ -99,27 +107,41 @@ class Market(Process):
         with self.mutex :
             if (self.conditions_meteo.value < 12) or (self.conditions_meteo.value > 30) :
                 self.sign_meteo = 1  
-        self.prixWattactuel = 0.8 * self.prixWattavant + self.coefMeteo * self.sign_meteo + self.coefGuerre * self.sign_guerre + self.coefTension * self.sign_tension + self.coefPenurie * self.sign_carburant + self.coefDevise * self.sign_devise
+        self.prixWattactuel = 0.8 * self.prixWattavant + self.coefMeteo * self.sign_meteo + self.coefGuerre * self.sign_guerre + self.coefTension * self.sign_tension + self.coefPenurie * self.sign_carburant + self.coefDevise * self.sign_devise - self.coefBaisse * self.sign_baisse + self.coefStronks * self.sign_stonks
 
         print("Le prix d'un Watt pour ce jour est de ", self.prixWattactuel, "€.") 
         #print("prix av :", self.prixWattavant, "tension", self.sign_tension, "guerre", self.sign_guerre, "devise", self.sign_devise, "penurie", self.sign_carburant, "meteo", self.sign_meteo, self.conditions_meteo.value)
         self.prixWattavant = self.prixWattactuel
         self.sign_meteo = 0
+        self.sign_baisse = 0
+        self.sign_stonks = 0
 
     #Fonction qui remet du stock d'energie dans le market s'il n'y en a plus
-    def restock_energie():
+    def restock_energie(self):
         self.stock+=self.stockInit
 
 
     #Gestion des transactions
-    def transactions(self):
-        pass
-        #lock() pour chaque transaction
-        #si type = achat
-            #code
+    def transactions(self, t, msg):
+        #Décodage du message msg       
+        #pid, qte_energie =
 
-        #si type = vente
-             #codedevise
+        #Gestion : Home vend de l'energie
+        if t==2 :
+            with Lock() :
+                self.stock += qte_energie
+                #le prix de l'energie va baisser au jour suivant (car plus d'offre)
+                self.sign_baisse += 1
+
+        #Gestion : Home achete de l'energie
+        if t==3 : 
+            with Lock() :
+                if qte_energie > self.stock :
+                    restock_energie()
+                #le prix de l'energie va monter au jour suivant (car plus de demande)
+                self.sign_stonks += 1
+
+       
 
         #if stock < demande d'achat -> restock_energie()
 
@@ -139,23 +161,11 @@ class Market(Process):
                 #value = int(value_v.decode())
                 #appel fonction transaction(vente)
 
-    def vendreAuMaison(quantite_energie):
-        stockInit-quantite_energie
-
 
     def run(self):
         #Création d'une message queue qui communiquera avec Home
         mq_market = sysv_ipc.MessageQueue(self.cle, sysv_ipc.IPC_CREAT)
 
-        #creer un pool de threads qui vont gerer les messages queues avec les maisons
-
-        #try:
-        #Démarrage des proccessus fils
-        #politique = Politics()
-        #economique = Economics()
-        #politique.start()
-        #time.sleep(2)
-        #economique.start()
         i = 0
         while True :
             if(i<JOURS):
@@ -186,7 +196,35 @@ class Market(Process):
                     self.sign_devise = 0 
                     time.sleep(0.01)
 
-                
+                #barriere
+
+                #creer un pool de threads qui vont gerer les messages queues avec les maisons
+                if self.sign_transac == 1 :
+                    self.sign_transac = 0
+                    with concurrent.futures.ThreadPoolExecutor(max_workers = 3) as executor :
+                        print("Début des transactions avec market")
+                            
+                        #voir s'il y a messages dans la queue
+                        print("Checking des messages recus dans la queue Market")
+                        while True :
+                            try :
+                                msg, t = mq_market.receive(block=False)
+                                break
+                                #print("mesg", mq_market.current_messages)
+                            except sysv_ipc.BusyError:
+                                time.sleep(0.01)
+                                
+                            #si oui, identifier son type : si son type est valide on envoie le message à la fonction transactions
+                            if t == 1 or t == 2 :
+                                print("Message recu valide")
+                                executor.submit(self.transactions, t, msg)
+
+                            elif (mq_market.current_messages == 0):
+                                print("Fin des transactions")
+                                
+                #barriere
+                           
+
                 i += 1
                 time.sleep(3)
 
